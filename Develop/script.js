b407855d516ab423a54062c464ed4274c43528b3
Clark <joshua@DOOMBOT-2.local>
2023-06-15 15:49:59 -0500

    made chars an object with the items for uppercase, lowercase, numbers, and special

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,33 @@
+//
+//
+//
+//
+//
+//
+//
+var chars = {
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "0123456789",
+  special: "!@#$%^&*()-_=+<>?/~:;."
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
