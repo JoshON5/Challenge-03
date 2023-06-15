04aec2681c97da02e0d322ee898f2df2d5073e24
Clark <joshua@DOOMBOT-2.local>
2023-06-15 17:14:39 -0500

    added pseudo code and also created a Pchars array to have the mulitiple functions be checked for the password generator and to later add a condition if none are picked for an alert window to pop up saying to 'choose one to generate password'

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
-//
-//
-//
-//
-//
-//
-//
+//WHen the generate password button is clicked a prompt should appear with the password criteria
+//The length of the password must be a min. of 8 chars and max. of 128 chars
+//Criteria options should be uppercase, lowercase, numbers, and special chars
+//When user chooses a criteria it should be checked
+//for password to generate an alert for at least one criteria should be checked should appear
+//checked options will then be used to generate the password in an empty string
+//Password generated should pop up in a window or appear in the HTML element
 var chars = {
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   lowercase: "abcdefghijklmnopqrstuvwxyz",
@@ -14,7 +14,20 @@ var chars = {
 
 
 
-
+ let Pchars = [
+  function uppercase() {
+    return chars.uppercase[Math.floor(Math.random * chars.uppercase.length)];
+  },
+  function lowercase() {
+    return chars.lowercase[Math.floor(Math.random * chars.lowercase.length)];
+  },
+  function numbers() {
+    return chars.numbers[Math.floor(Math.random * chars.numbers.length)];
+  },
+  function special() {
+    return chars.numbers[Math.floor(Math.random * chars.special.length)];
+  }
+ ]
 
 
 
