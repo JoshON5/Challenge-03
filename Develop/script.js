480068272b174ea12e346c720cbf5873028cf2c7
Clark <joshua@DOOMBOT-2.local>
2023-06-16 09:40:21 -0500

    removed the enchilida concat of arrays and made it an array that would co ncat with each criteria array if confirmed. fixed the for loop to fit the syntax and added a return state ment after the for loop to bring it back with all the added arrays. also formatted to look cleaner

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,88 @@
 //for password to generate an alert for at least one criteria should be checked should appear
 //checked options will then be used to generate the password in an empty string
 //Password generated should pop up in a window or appear in the HTML element
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var spChars = ["~","@","!","#","$","%","^","&","*","(",")","_","=","+","{", "}", "[", "]", ";", ".", "<", ">", "?"]
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var spChars = [
+  "~",
+  "@",
+  "!",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "=",
+  "+",
+  "{",
+  "}",
+  "[",
+  "]",
+  ";",
+  ".",
+  "<",
+  ">",
+  "?",
+];
 
 var passLength = "";
 var passSpecial;
@@ -17,31 +95,65 @@ var passUpper;
 var passLower;
 
 function generatePassword() {
-var passLength = prompt("How long of a password do you require?");
-    while(passLength <= 8 || passLength >= 128) {
-      alert("Password must be between 8-128 characters")
-      var passLength = prompt("How long of a password do you require?");
-    }
-    var passUpper = confirm("Confirm if you would want to include uppercase characters in your password");
-    var passLower = confirm("Confirm if you would want to include lowercase characters in your password");
-    var passNum = confirm("Confirm if you would want to include numbers in your password");
-    var passSpecial = confirm("Confirm if you would want to include special characters in your password (i.e. *&^")
-    while(passUpper === false && passLower === false && passNum === false && passSpecial === false) {
-      alert("You must choose at least one criteria");
-      var passUpper = confirm("Confirm if you would want to include uppercase characters in your password");
-      var passLower = confirm("Confirm if you would want to include lowercase characters in your password");
-      var passNum = confirm("Confirm if you would want to include numbers in your password");
-      var passSpecial = confirm("Confirm if you would want to include special characters in your password (i.e. *&^)")  
-    }
-} 
-var enchilida = upperCase.concat("", lowerCase, numbers, spChars);
-console.log(enchilida)
-var randomPass = ""
-for (var i = 0; i < passLength; i++) {
-  randomPass = randomPass + enchilida[Math.floor(Math.random() * enchilida.length)];
-  console.log(randomPass)
+  var passLength = prompt("How long of a password do you require?");
+  while (passLength <= 8 || passLength >= 128) {
+    alert("Password must be between 8-128 characters");
+    var passLength = prompt("How long of a password do you require?");
+  }
+  var passUpper = confirm(
+    "Confirm if you would want to include uppercase characters in your password"
+  );
+  var passLower = confirm(
+    "Confirm if you would want to include lowercase characters in your password"
+  );
+  var passNum = confirm(
+    "Confirm if you would want to include numbers in your password"
+  );
+  var passSpecial = confirm(
+    "Confirm if you would want to include special characters in your password (i.e. *&^"
+  );
+  while (
+    passUpper === false &&
+    passLower === false &&
+    passNum === false &&
+    passSpecial === false
+  ) {
+    alert("You must choose at least one criteria");
+    var passUpper = confirm(
+      "Confirm if you would want to include uppercase characters in your password"
+    );
+    var passLower = confirm(
+      "Confirm if you would want to include lowercase characters in your password"
+    );
+    var passNum = confirm(
+      "Confirm if you would want to include numbers in your password"
+    );
+    var passSpecial = confirm(
+      "Confirm if you would want to include special characters in your password (i.e. *&^)"
+    );
+  }
+
+  var enchilida = [];
+  if (passUpper) {
+    enchilida = enchilida.concat(upperCase);
+  }
+  if (passLower) {
+    enchilida = enchilida.concat(lowerCase);
+  }
+  if (passNum) {
+    enchilida = enchilida.concat(numbers);
+  }
+  if (passSpecial) {
+    enchilida = enchilida.concat(spChars);
+  }
+  console.log(enchilida);
+
+  var passGen = "";
+  for (var i = 0; i < passLength; i++) {
+    passGen = passGen + enchilida[Math.floor(Math.random() * enchilida.length)];
+  }
+  return passGen;
 }
-    
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -52,7 +164,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
